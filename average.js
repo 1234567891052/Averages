a806bff951dbc7d1ca5c6a2011cf179e5e2e1a61
1234567891052 <63904463+1234567891052@users.noreply.github.com>
2021-01-18 12:02:02 +0530

    Fixed the bugs in the median function.

--- average.js
+++ average.js
@@ -1,4 +1,4 @@
-let array = [1, 2, 2, 1];
+let array = [8,69,64,74,58,9,33,91,90,12,13,80,51,19,60,47,49,52,15,88,88];
 function getMean() {
     let sum = 0; 
     for (let i = 0; i < array.length; i++){
@@ -8,18 +8,19 @@ function getMean() {
     console.log('The mean is: ' + mean); 
 }
 function getMedian() {
-    let median; 
-    array.sort(); 
-    let len = array.length;
-    if (len % 2 === 0) {
-        median = (array[len / 2] + array[len / 2 - 1]) / 2;
+    array.sort(function(a, b){return a-b});
+    let len = array.length;  
+    let median = 0; 
+
+    if(array.length % 2 === 0){
+        median = (array[len/2]+array[len/2 - 1])/2;  
     } else {
-        median = array[(len - 1) / 2]; 
+        median = array[(len - 1)/2]; 
     }
     console.log('The median is: ' + median); 
 }
 function getMode() {
-    array.sort(); 
+    let bool = false; 
     const frequencyTable = {}; 
     array.forEach(elem => frequencyTable[elem] = frequencyTable[elem] + 1 || 1); 
     let modes = []; 
@@ -30,16 +31,30 @@ function getMode() {
             maxFrequency = frequencyTable[key];
         } else if (frequencyTable[key] === maxFrequency) {
             modes.push(Number(key)); 
+            bool = true; 
         }
     }
     if (modes.length === Object.keys(frequencyTable).length) {
         modes = []; 
+        bool = false; 
     }
-    console.log('The mode is: ' + modes); 
+
+    if(bool === true){
+        console.log('The mode is: ' + modes); 
+    } else {
+        console.log('NO MODE'); 
+    }
+}
+function getRange(){
+    array.sort(function(a, b){return a-b});
+    let len = array.length; 
+    let range = array[len - 1] - array[0]; 
+    console.log('The range is: ' + range); 
 }
 function update() {
     getMean(); 
     getMedian(); 
-    getMode(); 
+    getMode();
+    getRange();  
 }
 update(); 
