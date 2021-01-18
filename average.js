206ac7f34bbcec96bc1d113788abf9af7b6270af
1234567891052 <63904463+1234567891052@users.noreply.github.com>
2021-01-18 08:35:52 +0530

    Finished the mode function;

--- average.js
+++ average.js
@@ -1,7 +1,4 @@
-// Need to create the mode function
-
-let array = [1, 2, 3, 4, 5];
-
+let array = [1, 2, 2, 1];
 function getMean() {
     let sum = 0; 
     for (let i = 0; i < array.length; i++){
@@ -10,7 +7,6 @@ function getMean() {
     let mean = sum / array.length; 
     console.log('The mean is: ' + mean); 
 }
-
 function getMedian() {
     let median; 
     array.sort(); 
@@ -22,15 +18,28 @@ function getMedian() {
     }
     console.log('The median is: ' + median); 
 }
-
 function getMode() {
-    
+    array.sort(); 
+    const frequencyTable = {}; 
+    array.forEach(elem => frequencyTable[elem] = frequencyTable[elem] + 1 || 1); 
+    let modes = []; 
+    let maxFrequency = 0; 
+    for (const key in frequencyTable) {
+        if (frequencyTable[key] > maxFrequency) {
+            modes = [Number(key)]; 
+            maxFrequency = frequencyTable[key];
+        } else if (frequencyTable[key] === maxFrequency) {
+            modes.push(Number(key)); 
+        }
+    }
+    if (modes.length === Object.keys(frequencyTable).length) {
+        modes = []; 
+    }
+    console.log('The mode is: ' + modes); 
 }
-
 function update() {
     getMean(); 
     getMedian(); 
     getMode(); 
 }
-
 update(); 
